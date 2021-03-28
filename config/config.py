b3c0a6a96e9cb6c007d310443f4416561630f13f
lijia <lijia0732@sina.com>
2021-03-28 17:31:58 +0800

    update config settings

--- config/config.py
+++ config/config.py
@@ -12,9 +12,16 @@ addr = ('127.0.0.1', 53)
 
 '''
     doh.pub
+    dns.google
     dns.alidns.com
+    dns.rubyfish.cn
     cloudflare-dns.com
 DoH server defines in https://tools.ietf.org/html/rfc1035
 https://{domain}/dns-query?dns={ base64(DNS wire data) }
 '''
 DoH = 'dns.alidns.com'
+# you should better configure this before you set this dns server as the local server  
+DoH_ip = '223.6.6.6'
+# DoH_ip = None
+# check the SSL cert or not
+verify = False
